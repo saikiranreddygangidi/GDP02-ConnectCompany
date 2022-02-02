468c8f1a6fb26ad52e2716811c8dc501995f745b
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2022-02-01 21:15:39 -0600

    commpleted the styling and scripting part of welcome.vue

--- client/src/components/views/welcome.vue
+++ client/src/components/views/welcome.vue
@@ -28,3 +28,105 @@
     </div>
   </div>
 </template>
+
+<script>
+// import PritiNavbar from "../components/PritiNavbar.vue";
+export default {
+  data() {
+    return {
+      items: [],
+      address: [],
+    };
+  },
+  methods: {},
+  name: "Home",
+  components: {
+    // PritiNavbar,
+  },
+};
+</script>
+<style scoped>
+.home-card-container {
+  background-color: #0093e9;
+  background-image: url("../../assets/welcome.jpg");
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  background-size: cover;
+  color: white;
+  padding: 0px 40px;
+  height: 100%;
+  width: 100%;
+}
+.home-card {
+  height: 96vh;
+}
+.content-container {
+  display: grid;
+  height: 100%;
+  grid-template-rows: 80% 20%;
+}
+.col1 {
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-size: cover;
+  color: white;
+  margin-left: 20%;
+  margin-top: 5%;
+
+  height: 300px;
+}
+.button {
+  background-color: #4caf50; /* Green */
+  color: white;
+  padding: 8px;
+  margin-left: 9px;
+  border-radius: 12px;
+}
+.search-bar {
+  display: grid;
+  grid-template-columns: 80% 20%;
+  width: 40%;
+  align-items: center;
+  padding: 8px;
+}
+.col2 {
+  display: grid;
+  grid-template-columns: 25% 25% 25% 25%;
+}
+.col2-divs {
+  padding: 10px;
+}
+.col2-reviews {
+  display: grid;
+  place-items: center;
+}
+.booking {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+}
+.column {
+  margin: 10px;
+  display: grid;
+  place-items: center;
+  text-align: center;
+}
+.feature-image {
+  height: 200px;
+}
+.feature-container {
+  width: 100%;
+  min-width: 100%;
+  padding: 20px 80px;
+}
+.add-property {
+  text-align: center;
+  padding: 80px;
+  background-color: #fafafa;
+}
+</style>
