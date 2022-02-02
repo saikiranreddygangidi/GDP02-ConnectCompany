8b1e6031eb42d21fd90b8eb2c087d7f6ad951269
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2022-02-01 21:27:37 -0600

    Updated the route->index for adminDashboard configuration

--- client/src/components/router/index.js
+++ client/src/components/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import WelcomeComponent from "../components/views/welcome.vue";
+import AdminDashboard from "../components/views/adminDashboard.vue";
 
 Vue.use(VueRouter);
 
@@ -14,6 +15,11 @@ const routes = [
         path: "/welcome",
         name: "welcome",
         component: WelcomeComponent,
+      },
+      {
+        path: "/adminDashboard",
+        name: "AdminDashboard",
+        component: AdminDashboard,
       },
 
 ];
