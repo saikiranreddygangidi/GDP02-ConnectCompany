10fdb6c0e8d66d1f809851d5791336aece618d2d
Nikhil Reddy Pannala <nikhilreddypannala.98@gmail.com>
2022-02-04 10:25:28 -0600

    Created user dashboard template

---
+++ client/src/components/views/userDashboard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container-fluid mt-4">
+    <h1 class="h1" style="background:white">User Dashboard</h1>
+    <b-alert :show="loading" variant="info">Loading...</b-alert>
+    <b-row>
+      <b-col>
+        <table class="table table-striped table-light">
+          <thead>
+            <tr>
+              <th>Company Id</th>
+              <th>Registered companies</th>
+              <th>No of events</th>
+              <!-- <th>Location</th> -->
+              <!-- <th>No of  events</th> -->
+              <th>&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="company in companies" :key="company.id">
+              <td>{{ company.id }}</td>
+              <td>{{ company.companyName }}</td>
+              <td>{{ company.noofevents }}</td>
+              <!-- <td>{{ company.companyLocation }}</td>
+              <td>{{ company.noofevents }}</td> -->
+              <td class="text-right">
+                <!-- <a href="#" @click.prevent="showPopup = !showPopup">Kow more</a> - -->
+                <!-- <a href="#" @click.prevent="deletePost(company)">Unregister</a> -->
+                <b-button @click="modalShow = !modalShow" class="me-3"
+                  >Know more</b-button
+                >
+
+                <b-modal
+                  v-model="modalShow"
+                  :title="company.companyName"
+                  ok-only
+                  ok-primary
+                  ok-what
+                  >There are two events happening in this company.{{
+                    company.companyName
+                  }}
+                  -{{ company.companyName }}</b-modal
+                >
+                <button class="btn btn-danger" @click="deletePost(company)">
+                  Unregister
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </b-col>
+      <b-col lg="3">
+        <b-card :title="'Register a Company'">
+          <form @submit.prevent="savePost">
+            <b-form-group label="Company Name">
+              <b-form-input
+                type="text"
+                class="m-2"
+                v-model="model.companyName"
+                placeholder="Enter a company name"
+              ></b-form-input>
+            </b-form-group>
+            <div>
+              <b-btn type="submit" variant="success">Register</b-btn>
+            </div>
+          </form>
+        </b-card>
+      </b-col>
+    </b-row>
+  </div>
+</template>
