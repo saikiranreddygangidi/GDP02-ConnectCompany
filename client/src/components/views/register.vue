f1907c152b8785c6520069a09acb9cbcf0cee5a2
Venkat Sai Jarugula <jarugulavenkat7@gmail.com>
2022-02-04 19:29:31 -0600

    Added methods to register in script tag

--- client/src/components/views/register.vue
+++ client/src/components/views/register.vue
@@ -83,6 +83,24 @@ export default {
       wizard: {},
     };
   },
- 
+  methods: {
+    register() {
+      console.log(this.input);
+      this.$axios
+        .post("/register", this.input)
+        .then((response) => {
+          console.log(response);
+          this.input.email = "";
+          this.input.username = "";
+          this.input.code = "";
+          this.wizard.register =
+            " successfully uploaded data. you can login now !";
+        })
+        .catch((err) => {
+          console.log(err);
+          this.wizard.register = " error in uploading data";
+        });
+    },
+  },
 };
 </script>
