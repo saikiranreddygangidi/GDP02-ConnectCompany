9a58c95b150ba30d933e39f84fa71cdd8ff1cb79
Nikhil Reddy Pannala <nikhilreddypannala.98@gmail.com>
2022-02-07 23:09:10 -0600

    Created login api without encryption of password

--- connectcompanyapi/app/Controllers/Http/AuthController.js
+++ connectcompanyapi/app/Controllers/Http/AuthController.js
@@ -39,6 +39,48 @@ class AuthController {
           });
         }
       }
+      async login({ request, response, auth }) {
+        const userinfo = request.only(["username", "code"]);
+        const rules = {
+          username: "required",
+          code: "required",
+        };
+    
+        const validation = await validate(userinfo, rules);
+    
+        if (validation.fails()) {
+          return response.badRequest({
+            error: {
+              status: 401,
+              message: "bad request, missing some required for connectcompony",
+              fields: validation.messages(),
+            },
+          });
+        }
+        
+        userinfo.code = decryptedData;
+        console.log("userinfo ", userinfo);
+        const user = await User.query()
+          .where("username", userinfo.username)
+          .where("code", userinfo.code)
+          .fetch();
+    
+        if (user.rows.length > 0) {
+          const user = await User.findBy({ username: userinfo.username });
+          console.log("--------------------",user);
+          let jwtToken = await auth.generate(user);
+    
+          return response.status(200).json({
+            message: "authenticated",
+            data: user,
+            token: jwtToken.token,
+          });
+        } else {
+          return response.status(500).json({
+            message: "unauthenticated user",
+          });
+        }
+      }
 }
 
 module.exports = AuthController
