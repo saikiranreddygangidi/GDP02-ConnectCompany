173322fb6b52dde14f15ef337f7bbd374977b3cf
Nikhil Reddy Pannala <nikhilreddypannala.98@gmail.com>
2022-02-07 22:27:58 -0600

    worked on axios and written script and style of user dashboard

--- client/src/components/views/userDashboard.vue
+++ client/src/components/views/userDashboard.vue
@@ -68,3 +68,57 @@
     </b-row>
   </div>
 </template>
+
+<script>
+//import api from '@/api'
+export default {
+  data() {
+    return {
+      loading: false,
+      modalShow: false,
+      companies: [],
+      model: {},
+    };
+  },
+  async created() {
+    this.refreshPosts();
+  },
+  methods: {
+    async refreshPosts() {
+      const username = this.$store.getters.userdetails.username;
+      await this.$axios
+        .get("/getAllCompaniesByEmail/" + username)
+        .then((response) => {
+          this.companies = response.data;
+        });
+    },
+    async open(id) {
+      if (id == 1) {
+        console.log("test");
+      }
+    },
+    async savePost() {
+      this.companies.push(this.model);
+      /*
+      if (this.model.id) {
+        //await api.updatePost(this.model.id, this.model)
+      } else {
+      //  await api.createPost(this.model)
+      }
+      this.model = {} // reset form
+      await this.refreshPosts()*/
+    },
+    async deletePost(company) {
+      if (confirm("Are you sure you want to unregister this company?")) {
+        // if we are editing a post we deleted, remove it from the form
+        if (this.model.companies.id === company.id) {
+          this.events.remove(company);
+          // this.model = {}
+        }
+        //  await api.deletePost(id)
+        //   await this.refreshPosts()
+      }
+    },
+  },
+};
+</script>
