25c6ca76cc5a45c083b492995ec1a828907507f5
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2022-02-07 22:43:00 -0600

    added configuration for frontend vue application

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Router from "./router";
+import axios from "axios";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.prototype.$axios = axios.create({
+  //baseURL: `${process.env.API_BASE_URL}`,
+  baseURL: "https://connectcompanyapi.herokuapp.com/connectcompany/api/v1",
+  //baseURL: "http://127.0.0.1:3333/connectcompany/api/v1",
+
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router: Router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
