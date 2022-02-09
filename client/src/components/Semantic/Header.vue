daa413df3108bb0b7d75ca420c41a9ca7a92a8a7
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2022-02-08 23:45:08 -0600

    added Header file with template that is basic struture

---
+++ client/src/components/Semantic/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="success">
+      <b-navbar-brand href="#">ConnectCompany</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        
+      
+        <b-navbar-nav v-if="isloggied()" class="nav-but">
+          <b-nav-text 
+            ><router-link to="/login" class="border"
+              ><button>Logout</button></router-link
+            ></b-nav-text
+          >
+        </b-navbar-nav>
+        
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
