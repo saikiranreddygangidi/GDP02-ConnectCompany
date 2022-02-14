3aaded515505d12183b15ace1f92d475646ab2a8
vivekd31 <drakshapallyvivek31@gmail.com>
2022-02-13 23:16:24 -0600

    Created Logout .vue

---
+++ client/src/components/views/Logout.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+</div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$store.dispatch("destroyToken").then((response) => {
+      console.log(response)
+      this.$router.push({ name: "login" });
+    });
+  },
+};
+</script>
