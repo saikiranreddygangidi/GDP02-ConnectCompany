de4ed0f524a1a4e818ca8ce46d3b4cfa46546b66
Nikhil Reddy Pannala <nikhilreddypannala.98@gmail.com>
2022-02-13 23:06:35 -0600

    Added unenrolled events api

--- connectcompanyapi/app/Controllers/Http/UserController.js
+++ connectcompanyapi/app/Controllers/Http/UserController.js
@@ -8,6 +8,14 @@ class UserController {
 
     return events[0];
   }
+
+  async getAllnotEnrolledEvents({ request, response, auth, params }) {
+    let uid = params.uid;
+    let events = await Database.raw("select * from events where id not in (select eventId from subscriptions where userId ="+uid+")");
+
+
+    return events[0];
+  }
 }
 
 module.exports = UserController;
