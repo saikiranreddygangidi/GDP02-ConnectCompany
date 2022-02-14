e5371d9d8a8d489d9b1281eba9ef2b4f812b9f06
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2022-02-13 22:54:31 -0600

    created a api used for user which display all the enrolled event

--- connectcompanyapi/app/Controllers/Http/UserController.js
+++ connectcompanyapi/app/Controllers/Http/UserController.js
@@ -2,6 +2,12 @@
 const Database = use("Database");
 class UserController {
 
+  async getAllEnrolledEvents({ params, request, response, auth }) {
+    let uid = params.uid;
+    let events = await Database.raw("select * from events where id in (select eventId from subscriptions where userId ="+uid+")");
+
+    return events[0];
+  }
 }
 
 module.exports = UserController;
