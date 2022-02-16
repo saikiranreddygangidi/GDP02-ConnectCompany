3f64c3608278e2a7962d51af20a07d21c1a4e620
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2022-02-16 09:49:42 -0600

    Updated Admin dashboard template with respective field and action need to work on script

--- client/src/components/views/adminDashboard.vue
+++ client/src/components/views/adminDashboard.vue
@@ -1,82 +1,117 @@
 <template>
-  <div class="container-fluid mt-4">
-    <h1 class="h1" style="background:white">
-      Admin Dashboard
-      <div class="btn-center">
-        <b-button variant="primary" @click="logout()">
-          Logout
-          <font-awesome-icon icon="sign-in-alt" class="m-t-4" />
-        </b-button>
-      </div>
-    </h1>
-    <b-alert :show="loading" variant="info">Loading...</b-alert>
-    <b-row>
-      <b-card
-        class="d-flex justify-content-center col-sm-3 "
-        style="margin-left : 40%"
-        :title="model.id ? 'Edit Company ID#' + model.id : 'Add New Company'"
-      >
-        <form @submit.prevent="savePost">
-          <b-form-group label="Company Name">
-            <b-form-input
-              type="text"
-              v-model="model.companyName"
-            ></b-form-input>
-          </b-form-group>
-          <b-form-group label="Company Location">
-            <b-form-input
-              rows="4"
-              v-model="model.companyLocation"
-            ></b-form-input>
-          </b-form-group>
-          <b-form-group label="Company UserName">
-            <b-form-input
-              rows="4"
-              v-model="model.companyUserName"
-            ></b-form-input>
-          </b-form-group>
-          <b-form-group label="Company email">
-            <b-form-input rows="4" v-model="model.email"></b-form-input>
-          </b-form-group>
-          <div>
-            <b-btn type="submit" variant="success">Save Company</b-btn>
-          </div>
-        </form>
+  <div>
+    <div class="container studContainer">
+      <h2 class="text-left mt-3">
+        Admin Dashboard
+
+        <div class="float-right">
+          <router-link to="/logout">
+            <b-button pill variant="danger" class="float-right">
+              Logout
+            </b-button>
+          </router-link>
+        </div>
+        
+        <div class="float-right" style="margin-left:80%">
+        
+          <router-link to="/addCompany">
+            <b-button pill variant="info" class="float-right">
+              Create Company
+              <font-awesome-icon icon="fa-solid fa-user-secret" class="ml-1" />
+            </b-button>
+          </router-link>
+        </div>
+      </h2>
+
+      <b-card no-body class="mb-3" v-if="events.length">
+        <b-card-header header-tag="header" class="p-0" role="tab">
+          <b-button
+            block
+            v-b-toggle.private-accordion
+            variant="info"
+            class="btn-lg text-left"
+            id="pvtEvent-btn"
+          >
+            All Events
+            <span>(Latest events)</span>
+            <font-awesome-icon
+              icon="chevron-circle-down"
+              class="mt-1 ml-1 float-right"
+              id="arrow-down0"
+            />
+            <font-awesome-icon
+              icon="chevron-circle-up"
+              class="mt-1 ml-1 float-right d-none"
+              id="arrow-up0"
+            />
+          </b-button>
+        </b-card-header>
+        <b-collapse
+          id="private-accordion"
+          visible
+          accordion="private-accd"
+          role="tabpane2"
+        >
+          <b-card-body>
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Event Name</th>
+                  <th scope="col">Event Type</th>
+                  <th scope="col">Event Location</th>
+                  <th scope="col">EventDate</th>
+                  <th scope="col">Open Company</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="event in events" :key="event.id">
+                  <td>{{ event.eventName }}</td>
+                  <td>{{ event.eventType }}</td>
+                  <td>{{ event.eventLocation }}</td>
+                  <td>{{ event.eventDate }}</td>
+                  <td
+                    @click="
+                      $bvModal.show('addModal');
+
+                      openCompany(event.id);
+                    "
+                  >
+                    <button type="button" class="btn btn-primary">
+                      company details
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </b-card-body>
+        </b-collapse>
       </b-card>
-    </b-row>
-    <br />
-    <b-row>
-      <b-col>
-        <table class=" table table-striped table-light">
-          <thead>
-            <tr>
-              <th>Company Id</th>
-              <th>Company</th>
-              <th>Location</th>
-              <th>Company User Name</th>
-              <th>Company email</th>
-              <th>&nbsp;</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="company in companies" :key="company.id">
-              <td>{{ company.id }}</td>
-              <td>{{ company.companyName }}</td>
-              <td>{{ company.companyLocation }}</td>
-              <td>{{ company.companyUserName }}</td>
-              <td>{{ company.email }}</td>
-              <td class="text-right">
-                <a href="#" @click.prevent="populatePostToEdit(company)"
-                  >Edit</a
-                >
-                -
-                <a href="#" @click.prevent="deletePost(company)">Delete</a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </b-col>
-    </b-row>
+      <b-modal id="addModal" hide-footer centered>
+        <template #modal-title>
+          Company details
+        </template>
+        <div
+          class="d-flex flex-column justify-content-center align-items-center"
+        >
+          <div>
+            <!-- <label for="select event">Select Event</label> -->
+            <span> Company Name : {{ cdetails.companyName }} </span>
+          </div>
+          <div>
+            <span> company Location : {{ cdetails.companyLocation }} </span>
+          </div>
+
+          <div>
+            <b-button
+              class="mt-3 col-12"
+              block
+              @click="$bvModal.hide('addModal')"
+              >ok</b-button
+            >
+          </div>
+        </div>
+      </b-modal>
+    </div>
   </div>
 </template>
 <script>
