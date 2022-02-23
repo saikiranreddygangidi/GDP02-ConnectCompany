4b63f2e42abc7bd7a42ec95a5bc6db5d1fd2bdb1
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2022-02-23 10:14:25 -0600

    updated even schema file

--- connectcompanyapi/database/migrations/1638490661040_events_schema.js
+++ connectcompanyapi/database/migrations/1638490661040_events_schema.js
@@ -4,7 +4,25 @@
 const Schema = use('Schema')
 
 class EventsSchema extends Schema {
-  
+  up () {
+    this.create('events', (table) => {
+      table.increments().primary().index('id');
+      table.string('eventName', 100).notNullable();
+      table.string('eventType').notNullable();
+      table.string('eventLocation').notNullable();
+      table.integer("belongsTo").unsigned().notNullable();
+      table.foreign('belongsTo').references('id').inTable('companies').onDelete('cascade');
+      table.integer("createdBy").unsigned().notNullable();
+      table.foreign('createdBy').references('id').inTable('users').onDelete('cascade');
+      table.date('eventDate').notNullable();
+      
+      table.timestamps();
+    })
+  }
+
+  down () {
+    this.drop('events')
+  }
 }
 
 module.exports = EventsSchema
