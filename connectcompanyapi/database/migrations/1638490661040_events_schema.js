be267135e59afa48ae233e090d94b9d811c2f739
saikiranreddygangidi <saikiranreddygangidi@users.noreply.github.com>
2022-02-23 16:15:49 +0000

    Prettified Code!

--- connectcompanyapi/database/migrations/1638490661040_events_schema.js
+++ connectcompanyapi/database/migrations/1638490661040_events_schema.js
@@ -4,24 +4,32 @@
 const Schema = use("Schema");
 
 class EventsSchema extends Schema {
-  up () {
-    this.create('events', (table) => {
-      table.increments().primary().index('id');
-      table.string('eventName', 100).notNullable();
-      table.string('eventType').notNullable();
-      table.string('eventLocation').notNullable();
+  up() {
+    this.create("events", (table) => {
+      table.increments().primary().index("id");
+      table.string("eventName", 100).notNullable();
+      table.string("eventType").notNullable();
+      table.string("eventLocation").notNullable();
       table.integer("belongsTo").unsigned().notNullable();
-      table.foreign('belongsTo').references('id').inTable('companies').onDelete('cascade');
+      table
+        .foreign("belongsTo")
+        .references("id")
+        .inTable("companies")
+        .onDelete("cascade");
       table.integer("createdBy").unsigned().notNullable();
-      table.foreign('createdBy').references('id').inTable('users').onDelete('cascade');
-      table.date('eventDate').notNullable();
-      
+      table
+        .foreign("createdBy")
+        .references("id")
+        .inTable("users")
+        .onDelete("cascade");
+      table.date("eventDate").notNullable();
+
       table.timestamps();
-    })
+    });
   }
 
-  down () {
-    this.drop('events')
+  down() {
+    this.drop("events");
   }
 }
 
