3eff52060299aaf77c8e4465ef8c9c2d2c858e8b
vivekd31 <vivekd31@users.noreply.github.com>
2022-02-23 16:24:57 +0000

    Prettified Code!

--- connectcompanyapi/app/Controllers/Http/CompanUserController.js
+++ connectcompanyapi/app/Controllers/Http/CompanUserController.js
@@ -1,11 +1,11 @@
 "use strict";
 
 class CompanUserController {
-    async getAllEvents({ request, response, auth }) {
-        const events = await Database.select("*").from("events");
-    
-        return events;
-      }
+  async getAllEvents({ request, response, auth }) {
+    const events = await Database.select("*").from("events");
+
+    return events;
+  }
 }
 
 module.exports = CompanUserController;
