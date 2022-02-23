0348fa04cfd19b6d876bf4617e59597651323104
vivekd31 <drakshapallyvivek31@gmail.com>
2022-02-23 10:34:30 -0600

    Created Seeder file for user schema for giving sample data

---
+++ connectcompanyapi/database/seeds/EventSeeder.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| EventSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+class EventSeeder {
+  async run () {
+  }
+}
+
+module.exports = EventSeeder
