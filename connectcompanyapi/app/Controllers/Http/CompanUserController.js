bdb247e4561d6e53d2fd577d95ba6e96d668776c
vivekd31 <drakshapallyvivek31@gmail.com>
2022-02-23 10:24:31 -0600

    Created API to get all events for company users

--- connectcompanyapi/app/Controllers/Http/CompanUserController.js
+++ connectcompanyapi/app/Controllers/Http/CompanUserController.js
@@ -1,5 +1,11 @@
 "use strict";
 
-class CompanUserController {}
+class CompanUserController {
+    async getAllEvents({ request, response, auth }) {
+        const events = await Database.select("*").from("events");
+    
+        return events;
+      }
+}
 
 module.exports = CompanUserController;
