fe8501ba6063784caf13959a20472336c5092d04
vivekd31 <drakshapallyvivek31@gmail.com>
2022-03-05 22:40:46 -0600

    updated the main js file with bootstrap file package and added differen t comp

--- client/src/main.js
+++ client/src/main.js
@@ -1,19 +1,36 @@
 import Vue from "vue";
-import App from "./App.vue";
-import Router from "./router";
+import _ from "lodash";
 import axios from "axios";
+//import jquery from "jquery";
+import moment from 'moment';
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import JsonExcel from 'vue-json-excel';
+
+import App from "./App.vue";
+import router from "./router";
 import store from "./store";
 
+Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
-Vue.config.productionTip = false;
+library.add(faSpinner);
+library.add(fas);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.component('downloadExcel', JsonExcel);
+
+// Integrating jQuery with vue
+//window.$ = window.jQuery = jquery;
+
 
 new Vue({
-  router: Router,
+  router,
   store,
   render: (h) => h(App),
 }).$mount("#app");
