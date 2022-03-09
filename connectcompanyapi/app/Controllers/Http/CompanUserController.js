9243ea1e5ae909f51c09bb880669b0767afebdc3
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2022-03-08 18:24:04 -0600

    updated the companyUserController with add company major api for adding the company

--- connectcompanyapi/app/Controllers/Http/CompanUserController.js
+++ connectcompanyapi/app/Controllers/Http/CompanUserController.js
@@ -1,12 +1,13 @@
 "use strict";
-
-class CompanUserController {
+const Database = use("Database");
+const Mail = use("Mail");
+const Env = use("Env");
+class CompanyUserController {
   async getAllEvents({ request, response, auth }) {
     const events = await Database.select("*").from("events");
 
     return events;
   }
-
   async getAllEventsByUID({ params, request, response, auth }) {
     let uid = params.uid;
     const events = await Database.table("events")
@@ -15,6 +16,56 @@ class CompanUserController {
 
     return events;
   }
+
+  async addCompany({ request, response, auth }) {
+    try {
+      let company = request.post();
+      console.log(company);
+      let companyMember = {
+        username: company.CompanyMemberUserName,
+        email: company.CompanyMemberEmail,
+        role: "companyUser",
+        code: "*****",
+      };
+      let companydetail = {
+        companyLocation: company.companyLocation,
+        companyName: company.companyName,
+        userId: company.userId,
+        eventDate: company.eventDate,
+      };
+      let companymemberid = 0;
+      let companyid = 0;
+      try {
+        companymemberid = await Database.table("users").insert(companyMember);
+      } catch (err) {
+        console.log(err);
+      }
+      try {
+        companyid = await Database.table("companies").insert(companydetail);
+      } catch (err) {
+        console.log(err);
+      }
+
+      let useremail = Env.get("MAIL_USERNAME");
+
+      let cid =
+        "https://connectcompanysketch.herokuapp.com/#/updatePassword/" +
+        companymemberid;
+      console.log(cid);
+      console.log("here is the email", useremail);
+     
+
+      return response.json({
+        message: "succefully uploaded with id " + companyid,
+      });
+    } catch (err) {
+      console.log("err  in catch", err);
+      response.status(400).json({
+        err: err,
+      });
+    }
+  }
+  
 }
 
-module.exports = CompanUserController;
+module.exports = CompanyUserController;
