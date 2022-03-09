055d8d0c2da24f05304c9f9284c31467e69d0e20
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2022-03-08 18:31:42 -0600

    updated app.vue with some required attributes and updated with created mutations

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="body-height">
+      <Header />
+      <router-view />
+      <Footer />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Semantic/Header.vue";
+import Footer from "./components/Semantic/Footer.vue";
 
 export default {
-  name: "App",
+  name: "app",
   components: {
-    HelloWorld,
+    Header,
+    Footer,
+  },
+  created() {
+    let token = sessionStorage.getItem("access_token");
+    this.$axios.defaults.headers.common["Authorization"] = token;
+    // we should authorize token here by making API call if the token is invalid then clear localStorage and redirect to login page
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  background: url("./assets/home.jpg")
+    no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+* {
+  font-size: 14px;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.body-height {
+  min-height: 100vh;
+}
+
+.nw-color {
+  color: #006747;
+}
+
+.bg-nw {
+  background-color: #006747 !important;
+}
+
+.footer-bottom {
+  margin-bottom: 62px;
+}
+
+.vue-form-wizard .wizard-header {
+  display: none;
+}
+
+.vue-form-wizard .wizard-tab-content {
+  padding: 20px 20px !important;
+  margin-top: 10px;
+  border-top: 1px solid #ccc;
+}
+
+.fw-500 {
+  font-weight: 500;
+}
+
+.fw-600 {
+  font-weight: 600;
+}
+
+.fw-700 {
+  font-weight: 700;
 }
 </style>
