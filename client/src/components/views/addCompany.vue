b217b53c9d45b507489e833f3d56966552ec8747
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2022-03-18 14:03:19 -0500

    updated addcompany file

--- client/src/components/views/addCompany.vue
+++ client/src/components/views/addCompany.vue
@@ -1,54 +1,55 @@
+
+
 <template>
+ <b-container fluid class="container">
   <div class="container-fluid mt-4">
     <h2 class="text-left mt-3">
       Admin Dashboard
 
       <div class="float-right">
         <router-link to="/logout">
-          <b-button
-            pill
-            variant="danger"
-            class="float-right"
-            style="margin-left: 50px"
-          >
+          <b-button pill variant="danger" class="float-right" style="margin-left:50px">
             Logout
           </b-button>
         </router-link>
-
-        <router-link to="/adminDashboard">
-          <b-button
-            pill
-            variant="info"
-            class="float-right"
-            style="margin-left: 20px"
-          >
-            dashboard
-            <font-awesome-icon icon="fa-solid fa-user-secret" class="ml-1" />
-          </b-button>
-        </router-link>
+        
+          <router-link to="/adminDashboard">
+            <b-button pill variant="info" class="float-right" style="margin-left:20px">
+              dashboard
+              <font-awesome-icon icon="fa-solid fa-user-secret" class="ml-1" />
+            </b-button>
+          </router-link>
+        
       </div>
     </h2>
     <b-alert :show="loading" variant="info">Loading...</b-alert>
-    <b-row>
+    
+      </div>
+     
+      <b-row>
+        
+      <b-col></b-col>
+      <b-col lg="6" md="8" sm="10" >
       <b-card
-        class="d-flex justify-content-center col-sm-3"
-        style="margin-left: 40%"
+        
+        
+        
       >
-        <form @submit.prevent="savePost">
+        <b-form @submit.prevent="savePost">
           <b-form-group label="Company Name">
             <b-form-input
               type="text"
               v-model="companies.companyName"
             ></b-form-input>
           </b-form-group>
-
+          
           <b-form-group label="CompanyMemeber Email">
             <b-form-input
               type="email"
               v-model="companies.CompanyMemberEmail"
             ></b-form-input>
           </b-form-group>
-
+          
           <b-form-group label="Company Member Username">
             <b-form-input
               v-model="companies.CompanyMemberUserName"
@@ -71,11 +72,13 @@
           <div>
             <b-btn type="submit" variant="success">Save Company</b-btn>
           </div>
-        </form>
+        </b-form>
       </b-card>
-    </b-row>
-    <br />
-  </div>
+      </b-col>
+      <b-col></b-col>
+      </b-row>
+      </b-container>
+  
 </template>
 <script>
 //import api from '@/api'
@@ -92,7 +95,7 @@ export default {
   methods: {
     savePost() {
       console.log(this.$store.getters.userDetails.id, "djfhgdwfhghjn");
-
+      
       this.companies.userId = this.$store.getters.userDetails.id;
       this.$axios
         .post("/addCompany", this.companies)
@@ -110,7 +113,7 @@ export default {
           this.spin = false;
           console.log("----", error.response.data);
         });
-      this.companies = {};
+        this.companies={}
     },
 
     logout() {
