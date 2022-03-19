b48073c4b685d72322d91caa436a8b88054359fc
vivekd31 <vivekd31@users.noreply.github.com>
2022-03-19 02:41:33 +0000

    Prettified Code!

--- connectcompanyapi/app/Controllers/Http/CompanUserController.js
+++ connectcompanyapi/app/Controllers/Http/CompanUserController.js
@@ -31,14 +31,21 @@ class UserController {
 
   async getAllEnrolledEvents({ params, request, response, auth }) {
     let uid = params.uid;
-    let events = await Database.raw("select * from events where id in (select eventId from subscriptions where userId ="+uid+")");
+    let events = await Database.raw(
+      "select * from events where id in (select eventId from subscriptions where userId =" +
+        uid +
+        ")"
+    );
 
     return events[0];
   }
   async getAllnotEnrolledEvents({ request, response, auth, params }) {
     let uid = params.uid;
-    let events = await Database.raw("select * from events where id not in (select eventId from subscriptions where userId ="+uid+")");
-
+    let events = await Database.raw(
+      "select * from events where id not in (select eventId from subscriptions where userId =" +
+        uid +
+        ")"
+    );
 
     return events[0];
   }
